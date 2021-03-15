c6234b2fc697da2e87fd3370294a0c76640d812a
ApurboStarry <hasinapurbo984@gmail.com>
2021-03-15 12:39:44 +0600

    Video player is responsive as well as the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import NavBar from './components/navbar';
 import Video from './components/video';
 
 function App() {
   return (
     <div className="App">
-      <Video source="http://192.168.31.173:8000/video" />
+      <NavBar />
+      <Video source="http://192.168.31.173:8000/x-video?filePath=/media/apurbo/A69A97279A96F353/Procrastination/Christopher Nolan Movies/Interstellar (2014) (2014) [1080p]/Interstellar.2014.2014.1080p.BluRay.x264.YIFY.mp4" />
     </div>
   );
 }
